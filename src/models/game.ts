266afa628e1b4381ae1dd3308de5c708f8bee8c5
Christian Hansen <68639582+christian-hansen@users.noreply.github.com>
2023-05-06 19:53:31 +0200

    material design button

--- src/models/game.ts
+++ src/models/game.ts
@@ -1,5 +1,5 @@
 export class Game {
-    public players: string[] = [];
+    public players: string[] = ['Spieler 1', 'Spieler 2', 'Spieler 3'];
     public stack: string[] = [];
     public playedCards: string[] = [];
     public currentPlayer: number = 0;
