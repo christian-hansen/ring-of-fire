1689e640fdeeb53eb47916f5ff8ab1b5098f8af8
Christian Hansen <68639582+christian-hansen@users.noreply.github.com>
2023-06-07 15:56:10 +0200

    load game

--- src/app/dialog-load-games/dialog-load-games.component.ts
+++ src/app/dialog-load-games/dialog-load-games.component.ts
@@ -1,28 +1,56 @@
 import { Component, inject } from '@angular/core';
-import { Firestore, collection, collectionData, doc, docData } from '@angular/fire/firestore';
+import {
+  CollectionReference,
+  DocumentData,
+  Firestore,
+  collection,
+  collectionData,
+} from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-dialog-load-games',
   templateUrl: './dialog-load-games.component.html',
-  styleUrls: ['./dialog-load-games.component.scss']
+  styleUrls: ['./dialog-load-games.component.scss'],
 })
 export class DialogLoadGamesComponent {
-
-  firestore: Firestore = inject(Firestore)
-  games$: any;
+  private gamesCollection: CollectionReference<DocumentData>;
+  allGameId: string[] = [];
+  allGamePlayers: string[] = [];
+  allGameLanguage: string[] = [];
+  allGames: any = [];
+  firestore: Firestore = inject(Firestore);
 
   constructor() {
-    const gamesCollection = collection(this.firestore, 'games');
-    this.games$ = collectionData(gamesCollection);
+    this.gamesCollection = collection(this.firestore, 'games');
 
-    this.games$.subscribe((games) => {
-      // with .subscribe we subscribe to the updates of this variable. We give the result (newTodos) into the function.
-      console.log('Games sind', games);
-    });
-  }
-    // console.log(this.games);
-    // console.log(gamesCollection);
+    collectionData(this.gamesCollection, { idField: 'id' }).subscribe(
+      (data) => {
+        data.forEach((game) => {
+          let gamesobject = [];
+          gamesobject['id'] = game['id'];
+          gamesobject['players'] = game['players'].length;
+          gamesobject['language'] = game['language'];
+          this.allGames.push(gamesobject);
+          console.log(this.allGames);
+        });
+      }
+    );
+    
     
   }
 
+  // this.games$.subscribe((games) => {
+  //   // with .subscribe we subscribe to the updates of this variable. We give the result (newTodos) into the function.
+  //   console.log('Games sind', games);
+  //   this.gameobjects = games;
 
+  //   this.route.params.subscribe(params => {
+  //     console.log(params['id'])
+  //     docData(doc(this.firestore, `games/${params['id']}`))
+  //   });
+  // });
+
+  // }
+  // console.log(this.games);
+  // console.log(gamesCollection);
+}
